963f916a9021cf11a05bf8d3d8ca93d4a1c45986
Ruslan <ruslanehg@gmail.com>
2021-07-25 21:34:31 +0600

    added profile page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'applications'
 urlpatterns = [
-    path('main/', views.MainPage.as_view()),
+    path('', views.MainPage.as_view(), name='main'),
     path('posts/<int:pk>/', views.PostDetail.as_view(), name='post.detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('profile/', views.ProfilePage.as_view(), name='profile'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 import random
+from django import forms
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import TemplateView
 from .models import *
 # Create your views here.
 
@@ -25,3 +29,14 @@ class PostDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class ProfilePage(TemplateView):
+    template_name = 'profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        subscribes = UserSubscription.objects.filter(user_id=self.request.user.id)
+        context['subscribes'] = subscribes
+        return context
+
